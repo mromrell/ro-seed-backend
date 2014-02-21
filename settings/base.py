f88b4677fccf1c8df0eed79e3cd1c5bd8cc6cb05
Michael Romrell <mromrell@live.com>
2014-02-20 17:25:26 -0700

    debug = false

--- settings/base.py
+++ settings/base.py
@@ -49,7 +49,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 # This should be set to the test domain (e.g. test.django-angular-pt.com)
-ALLOWED_HOSTS = ['my-empty-seed-project.herokuapp.com', 'my-empty-seed-project-be.herokuapp.com/']
+ALLOWED_HOSTS = ['my-empty-seed-project-be.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
